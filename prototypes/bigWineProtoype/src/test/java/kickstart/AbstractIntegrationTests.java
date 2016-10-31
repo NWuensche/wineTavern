0ae3797c56eec1f27eff20fec190bd5247e217cc
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-10-31 14:42:15 +0100

    Make AbstractIntegration tests abstract + delete double author

--- prototypes/bigWineProtoype/src/test/java/kickstart/AbstractIntegrationTests.java
+++ prototypes/bigWineProtoype/src/test/java/kickstart/AbstractIntegrationTests.java
@@ -1,9 +1,5 @@
 package kickstart;
 
-/**
- * Created by nwuensche on 31.10.16.
- */
-
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -18,5 +14,5 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
 @Transactional
-public class AbstractIntegrationTests {
+public abstract class AbstractIntegrationTests {
 }
