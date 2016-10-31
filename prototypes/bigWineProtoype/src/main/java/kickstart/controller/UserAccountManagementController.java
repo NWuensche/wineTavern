b8a8867d83af43da0e2f5553f7b208c948a95dc8
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-10-31 18:22:28 +0100

    Add controller to manage UserAccounts

---
+++ prototypes/bigWineProtoype/src/main/java/kickstart/controller/UserAccountManagementController.java
@@ -0,0 +1,36 @@
+package kickstart.controller;
+
+import kickstart.RegisterCredentials;
+import org.salespointframework.useraccount.UserAccount;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.salespointframework.useraccount.UserAccountManager;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+/**
+ * Controller to store a new User in the DB.
+ * @author michel
+ */
+
+@Controller
+public class UserAccountManagementController {
+
+    UserAccountManager userAccountManager;
+
+    @Autowired
+    public UserAccountManagementController(UserAccountManager userAccountManager) {
+        this.userAccountManager = userAccountManager;
+    }
+
+    @RequestMapping(value="/addNew", method=RequestMethod.POST)
+    public String index(@ModelAttribute(value="registercredentials") RegisterCredentials registerCredentials) {
+        UserAccount account = userAccountManager.create(registerCredentials.getName(), registerCredentials.getPassword());
+
+        userAccountManager.save(account);
+
+        return "welcome";
+    }
+}
