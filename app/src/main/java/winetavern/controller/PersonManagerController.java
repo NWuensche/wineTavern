139c93fddb682e88fd3a68abc2d36efe1167d661
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-11-04 21:06:40 +0100

    rename PersonManagerController and delete wrong methods

--- app/src/main/java/winetavern/controller/PersonManagerController.java
+++ app/src/main/java/winetavern/controller/PersonManagerController.java
@@ -3,23 +3,10 @@ package winetavern.controller;
 import winetavern.model.user.Person;
 
 /**
- * Manage all persons, which are connected with the wine tavern.
- * Basically a UserAccountManager Decorator
+ * Controller, which maps {@link Person} related stuff
  * @author Niklas Wünsche
  */
 
-public class PersonManager {
-
-    public void addPerson(Person person) {
-
-    }
-
-    public void removePerson(Person person) throws Exception { // TODO Which Exception in UserAccountManager
-
-    }
-
-    public void getAdress(Person person) {
-
-    }
+public class PersonManagerController {
 
 }
