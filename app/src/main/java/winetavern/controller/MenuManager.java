82782c85913d495c56af63e2ce86bea50504521c
Michel Kunkler <michel.kunkler@gmail.com>
2016-11-05 00:51:14 +0100

    creating templates

--- app/src/main/java/winetavern/controller/MenuManager.java
+++ app/src/main/java/winetavern/controller/MenuManager.java
@@ -10,21 +10,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MenuManager {
     @RequestMapping("/admin/showMenus")
     public String showMenus() {
-
+        return "daymenulist";
     }
 
     @RequestMapping("/admin/addMenu")
     public String addMenu() {
-
+        return "daymenulist";
     }
 
     @RequestMapping("/admin/removeMenu")
     public String removeMenu() {
-
+        return "daymenulist";
     }
 
     @RequestMapping("/admin/getDayMenuByDay")
     public String getDayMenuByDay() {
-
+        return "daymenu";
     }
 }
