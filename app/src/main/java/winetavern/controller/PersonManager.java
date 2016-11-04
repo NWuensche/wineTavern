7a44fb5f5aef681a1e437324ff38f945db002d7d
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-11-04 15:14:44 +0100

    Move PersonManager from models to controller + add empty methods

---
+++ app/src/main/java/winetavern/controller/PersonManager.java
@@ -0,0 +1,25 @@
+package winetavern.controller;
+
+import winetavern.model.user.Person;
+
+/**
+ * Manage all persons, which are connected with the wine tavern.
+ * Basically a UserAccountManager Decorator
+ * @author Niklas Wünsche
+ */
+
+public class PersonManager {
+
+    public void addPerson(Person person) {
+
+    }
+
+    public void removePerson(Person person) throws Exception { // TODO Which Exception in UserAccountManager
+
+    }
+
+    public void getAdress(Person person) {
+
+    }
+
+}
