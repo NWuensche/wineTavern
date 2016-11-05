d59b05e05910ca804d05ce1df3419a21ba0cf377
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-11-05 15:12:34 +0100

    Fix WebIntegrationTests
    Fixed the Application and WelcomeController tests, which had to login to procced. Also polish everything and move some tests in the right class.

--- app/src/test/java/winetavern/ApplicationTests.java
+++ app/src/test/java/winetavern/ApplicationTests.java
@@ -1,12 +1,9 @@
 package winetavern;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import winetavern.controller.LoginRequests;
 import org.junit.Test;
-import org.salespointframework.useraccount.UserAccountManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.RequestBuilder;
 
 /**
  * Tests for Data initialization and login behavior
@@ -15,28 +12,10 @@ import org.springframework.test.web.servlet.RequestBuilder;
 
 public class ApplicationTests extends AbstractWebIntegrationTests {
 
-    @Autowired UserAccountManager userAccountManager;
-    @Autowired Application application;
-
     @Test
-    public void redirectsAfterLogin() throws Exception {
-        RequestBuilder adminLoginRequest = LoginRequests.getLoginRequest("admin", "1234");
-        mvc.perform(adminLoginRequest)
+    public void redirectIfNotLoggedIn() throws Exception{
+        mvc.perform(get("/"))
                 .andExpect(status().is3xxRedirection());
-                // TODO NoModelAndView Exception?
-                //.andExpect(model().attributeExists("accountcredentials"))
-                //.andExpect(view().name("welcome"));
-    }
-
-
-    @Test
-    public void errorAfterWrongLogin() throws Exception{
-        RequestBuilder wrongLoginRequest = LoginRequests.getLoginRequest("admin", "wrong pass");
-        mvc.perform(wrongLoginRequest)
-                .andExpect(status().is3xxRedirection());
-                // TODO NoModelAndView Exception?
-                //.andExpect(model().attributeDoesNotExist("accountcredentials"))
-                //.andExpect(view().name("login"));
     }
 
 }
