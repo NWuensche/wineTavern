e487e1b5790210902358e4dd0e3002b2ff3527cd
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-11-05 11:45:08 +0100

    Fix Exceptions in Tests

--- app/src/test/java/winetavern/model/reservation/TableTests.java
+++ app/src/test/java/winetavern/model/reservation/TableTests.java
@@ -1,22 +1,22 @@
 package winetavern.model.reservation;
 
-import com.sun.javaws.exceptions.InvalidArgumentException;
-import com.sun.xml.internal.ws.api.message.ExceptionHasMessage;
 import org.junit.Test;
 import winetavern.AbstractWebIntegrationTests;
 
 /**
  * @author Sev
  */
+
 public class TableTests extends AbstractWebIntegrationTests{
 
-    @Test(expected = InvalidArgumentException.class)
-    public void TableContructorNegativeCapacityTest() throws Exception{
+    @Test(expected = IllegalArgumentException.class)
+    public void TableContructorNegativeCapacityTest() {
         Table table = new Table(-2,1);
     }
 
-    @Test(expected = InvalidArgumentException.class)
-    public void TableContructorNegativeNumberTest() throws Exception{
+    @Test(expected = IllegalArgumentException.class)
+    public void TableContructorNegativeNumberTest() {
         Table table = new Table(5,-3);
     }
+
 }
