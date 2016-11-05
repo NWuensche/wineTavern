24ae5b36e0fdc451c4f8cce2ef520b647f969b8e
SEV <severin.engert@mailbox.tu-dresden.de>
2016-11-05 09:39:17 +0100

    fixed conflicts, michels menu isnt working

--- app/src/main/java/winetavern/model/reservation/Table.java
+++ app/src/main/java/winetavern/model/reservation/Table.java
@@ -1,7 +1,44 @@
 package winetavern.model.reservation;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
- * Created by Louis on 04.11.2016.
+ * @author Sev
  */
+@Entity(name = "restaurant_table")
 public class Table {
+
+    private @Id @GeneratedValue long id;
+
+    private int capacity;
+    private int number;
+
+
+    public Table(int capacity,int number) throws InvalidArgumentException{
+        if(capacity <= 0){throw new InvalidArgumentException(new String[]{"No Table should have capacity <= 0"});}
+        if(number <= 0){throw new InvalidArgumentException(new String[]{"No Table should be identified by number <= " +
+                "0"});}
+        this.number = number;
+        this.capacity = capacity;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }
