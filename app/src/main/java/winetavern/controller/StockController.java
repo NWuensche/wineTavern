b8ab9005810d607658c8ee34676eb0372f462aa5
Fritz Louis Wilke <anmeldungs.mail@web.de>
2016-11-07 19:10:25 +0100

    added StockController

---
+++ app/src/main/java/winetavern/controller/StockController.java
@@ -0,0 +1,64 @@
+package winetavern.controller;
+
+import org.salespointframework.catalog.Product;
+import org.salespointframework.catalog.ProductIdentifier;
+import org.salespointframework.inventory.Inventory;
+import org.salespointframework.inventory.InventoryItem;
+import org.salespointframework.quantity.Quantity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import winetavern.model.stock.ProductCatalog;
+
+import java.util.Optional;
+
+/**
+ * @author Louis
+ */
+@Controller
+public class StockController {
+    private final Inventory<InventoryItem> stock;
+    private final ProductCatalog products;
+
+    @Autowired
+    public StockController(Inventory<InventoryItem> stock, ProductCatalog products) {
+        this.stock = stock;
+        this.products = products;
+    }
+
+    @RequestMapping("/admin/stock")
+    public String manageStock() {
+        stock.findAll();
+        return "stock";
+    }
+
+    @RequestMapping(value = "/admin/stock/add", method = RequestMethod.POST)
+    public String addProduct(@ModelAttribute("product") Product product, @ModelAttribute("quantity") Quantity quantity) {
+        stock.save(new InventoryItem(product, quantity));
+        return "redirect:/admin/stock";
+    }
+
+    @RequestMapping(value = "/admin/stock/increaseQuantity", method = RequestMethod.POST)
+    public String increaseQuantity(@ModelAttribute("product") ProductIdentifier productId,
+                                   @ModelAttribute("quantity") String quantityNumber) {
+        Optional<Product> product = products.findOne(productId);
+        if (product.isPresent()) {
+            Optional<InventoryItem> item = stock.findByProduct(product.get());
+            if (item.isPresent()) item.get().increaseQuantity(Quantity.of(new Integer(quantityNumber)));
+        }
+        return "redirect:/admin/stock";
+    }
+
+    @RequestMapping(value = "/admin/stock/decreaseQuantity", method = RequestMethod.POST)
+    public String decreaseQuantity(@ModelAttribute("product") ProductIdentifier productId,
+                                   @ModelAttribute("quantity") String quantityNumber) {
+        Optional<Product> product = products.findOne(productId);
+        if (product.isPresent()) {
+            Optional<InventoryItem> item = stock.findByProduct(product.get());
+            if (item.isPresent()) item.get().decreaseQuantity(Quantity.of(new Integer(quantityNumber)));
+        }
+        return "redirect:/admin/stock";
+    }
+}
