dbe57812663c6a85be1729ea781a60d727630d21
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-11-08 20:31:01 +0100

    Add RepositoryManager test and save table

--- app/src/main/java/winetavern/model/reservation/ReservationRepository.java
+++ app/src/main/java/winetavern/model/reservation/ReservationRepository.java
@@ -2,9 +2,13 @@ package winetavern.model.reservation;
 
 import org.salespointframework.core.SalespointRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 /**
  * Created by nwuensche on 05.11.16.
  */
 
 public interface ReservationRepository extends SalespointRepository<Reservation, Long> {
+    public List<Reservation> findByTime(LocalDateTime time);
 }
