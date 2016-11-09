b3e2b2885c1f5e33fd0f020a790f5cb49ae9da0f
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-11-09 17:26:51 +0100

    Delete ScheduleRepository

--- app/src/main/java/winetavern/model/management/ScheduleRepository.java
+++
@@ -1,9 +0,0 @@
-package winetavern.model.management;
-
-import org.salespointframework.core.SalespointRepository;
-
-/**
- * Created by nwuensche on 09.11.16.
- */
-/*public interface ScheduleRepository extends SalespointRepository<Schedule, Long> {
-}*/
