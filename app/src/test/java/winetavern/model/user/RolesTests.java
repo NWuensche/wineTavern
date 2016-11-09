92d1b276823224de0b9f4ce766ba0cbd41c1e223
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-11-09 18:22:57 +0100

    Add tests for Roles Enum

--- app/src/test/java/winetavern/model/user/RolesTests.java
+++ app/src/test/java/winetavern/model/user/RolesTests.java
@@ -1,7 +1,23 @@
 package winetavern.model.user;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+
+import org.junit.Test;
+import org.salespointframework.useraccount.Role;
+
 /**
- * Created by nwuensche on 09.11.16.
+ * Tests for {@link Roles}
  */
+
 public class RolesTests {
+
+    @Test
+    public void isAdminDataRight() {
+        Roles admin = Roles.ADMIN;
+        assertThat(admin.getRole(), is(Role.of("ROLE_ADMIN")));
+        assertThat(admin.getNameOfRoleWithPrefix(), is("ROLE_ADMIN"));
+        assertThat(admin.getRealNameOfRole(), is("ADMIN"));
+    }
+
 }
