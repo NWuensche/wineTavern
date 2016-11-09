31fc68cae4946a9a5a043a02d7af5593699c13b7
Louis Wilke <anmeldungs.mail@web.de>
2016-11-09 10:20:45 +0100

    added BusinessTime to templates: available under "businessTime"

---
+++ app/src/main/java/winetavern/controller/GlobalAttributesController.java
@@ -0,0 +1,30 @@
+package winetavern.controller;
+
+import org.salespointframework.time.BusinessTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+/**
+ * @author Louis
+ */
+
+@ControllerAdvice
+public class GlobalAttributesController {
+    private final BusinessTime businessTime;
+
+    @Autowired
+    public GlobalAttributesController(BusinessTime businessTime) {
+        this.businessTime = businessTime;
+    }
+
+    @ModelAttribute
+    public void setBusinessTime(Model model) {
+        model.addAttribute("businessTime", businessTime.getTime().format(DateTimeFormatter.
+                ofLocalizedDateTime(FormatStyle.SHORT)));
+    }
+}
