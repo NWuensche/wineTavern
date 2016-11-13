09aaa8dc17e57d3511473e29ceb0e3bf19ac28dc
Katharina Letz <letz.katharina@gmail.com>
2016-11-13 17:12:16 +0100

    Add sidebar toggle effect

---
+++ app/src/main/resources/static/resources/js/script.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){
+
+    // click sidebar toggle
+    $('#sidebarToggle').on('click', function(e){
+        e.preventDefault();
+        $('body').toggleClass('mini');
+    });
+
+});
