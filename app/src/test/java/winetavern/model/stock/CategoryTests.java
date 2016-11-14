84579dd5f3e66077513658aac729e30544b0df54
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-11-14 14:04:18 +0100

    Add category enum with standard functions

---
+++ app/src/test/java/winetavern/model/stock/CategoryTests.java
@@ -0,0 +1,9 @@
+package winetavern.model;
+
+import winetavern.AbstractWebIntegrationTests;
+
+/**
+ * Created by nwuensche on 14.11.16.
+ */
+public class CategoryTests extends AbstractWebIntegrationTests {
+}
