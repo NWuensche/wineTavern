421abe3688b753691722a0a15c51808f529337a1
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-11-14 14:27:39 +0100

    Add tests for category enum

--- app/src/test/java/winetavern/model/stock/CategoryTests.java
+++ app/src/test/java/winetavern/model/stock/CategoryTests.java
@@ -1,9 +1,30 @@
-package winetavern.model;
+package winetavern.model.stock;
 
-import winetavern.AbstractWebIntegrationTests;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+
+import org.junit.Test;
+
 
 /**
  * Created by nwuensche on 14.11.16.
  */
-public class CategoryTests extends AbstractWebIntegrationTests {
+public class CategoryTests {
+
+    @Test
+    public void isRedWineDataRight() {
+        Category redWine = Category.RED_WINE;
+
+        assertThat(redWine.getCategoryName(), is("RED_WINE"));
+        assertThat(Category.getDisplayNameCategory(Category.RED_WINE), is("Rotwein"));
+    }
+
+    @Test
+    public void isSnackDataRight() {
+        Category snack = Category.SNACK;
+
+        assertThat(snack.getCategoryName(), is("SNACK"));
+        assertThat(Category.getDisplayNameCategory(Category.SNACK), is("Snacks"));
+    }
+
 }
