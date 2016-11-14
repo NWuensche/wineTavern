7028021a3e3d3e56a0899f02c7a2f4614b97aa00
Louis Wilke <anmeldungs.mail@web.de>
2016-11-14 23:00:28 +0100

    refactored ShiftController

--- app/src/main/java/winetavern/controller/ShiftController.java
+++ app/src/main/java/winetavern/controller/ShiftController.java
@@ -8,14 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import winetavern.model.management.Shift;
 import winetavern.model.management.ShiftRepository;
 import winetavern.model.management.TimeInterval;
-import winetavern.model.user.Person;
-import winetavern.model.user.PersonManager;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * @author Louis
@@ -23,48 +20,35 @@ import java.util.TreeSet;
 
 @Controller
 public class ShiftController {
-    private final ShiftRepository shifts;
-    private final BusinessTime time;
-    @Autowired private PersonManager personManager;
-
-    @Autowired
-    public ShiftController(ShiftRepository shifts, BusinessTime time) {
-        this.shifts = shifts;
-        this.time = time;
-    }
+    @Autowired private ShiftRepository shifts;
+    @Autowired private BusinessTime time;
 
     @RequestMapping("/admin/management/shifts")
     public String showShifts(Model model) {
 
         TimeInterval week = getWeekInterval(time.getTime()); //get the week interval out of businessTime
-        Set<Shift> shiftsOfWeek = getShiftsOfWeek(week);     //get all shifts in this interval
+        List<Shift> shiftsOfWeek = getShiftsOfWeek(week);    //get all shifts in this interval
 
         model.addAttribute("weekStart", week.getStart().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
         model.addAttribute("weekEnd", week.getEnd().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
         model.addAttribute("shiftAmount", shiftsOfWeek.size());
-        model.addAttribute("shifts", shifts.findAll());
+        model.addAttribute("shifts", shiftsOfWeek);
 
         return "shifts";
     }
 
     private TimeInterval getWeekInterval(LocalDateTime date) {
-        int day = date.getDayOfWeek().getValue();                                                  //returns 1-7
-        TimeInterval week = new TimeInterval(date.toLocalDate().atStartOfDay().minusDays(day - 1), //last Monday
-                date.withHour(23).withMinute(59).withSecond(59).plusDays(7 - day));                //next Sunday
-        return week;
+        int day = date.getDayOfWeek().getValue();                                     //returns 1-7
+        return new TimeInterval(date.toLocalDate().atStartOfDay().minusDays(day - 1), //last Monday
+                date.withHour(23).withMinute(59).withSecond(59).plusDays(7 - day));   //next Sunday
     }
 
-    private Set<Shift> getShiftsOfWeek(TimeInterval week) {
+    private List<Shift> getShiftsOfWeek(TimeInterval week) {
+        List<Shift> res = new LinkedList<>();
+        for (Shift shift : shifts.findAll())
+            if (TimeInterval.intersects(shift.getInterval(), week)) res.add(shift);
 
-        Set<Shift> res = new TreeSet<>();
-
-        for (Shift shift : shifts.findAll()) {
-            TimeInterval shiftTime = shift.getInterval();
-            if (shiftTime.getStart().compareTo(week.getStart()) == 1 || //start of shift in week
-                    shiftTime.getEnd().compareTo(week.getEnd()) == -1) //or end of shift in week
-                res.add(shift);
-        }
-
+        Collections.sort(res, (o1, o2) -> (o1.getInterval().getStart().compareTo(o2.getInterval().getStart())));
         return res;
     }
 }
