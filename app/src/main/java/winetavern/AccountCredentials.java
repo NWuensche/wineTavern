dc7c2b97ee840cb74ecaf8caff5cc229e1ab9f2f
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-11-18 15:32:44 +0100

    Add new options to AccountCredentials

--- app/src/main/java/winetavern/AccountCredentials.java
+++ app/src/main/java/winetavern/AccountCredentials.java
@@ -5,11 +5,33 @@ package winetavern;
  * @author michel
  */
 
+// TODO Make this an inner class of PersonManagerController
 public class AccountCredentials {
 
+    private String sex;
+    private String firstName;
+    private String lastName;
+    private String birthday;
     private String username;
     private String password;
-    private String role = "";
+    private String role;
+    private String address;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
 
     public String getUsername() {
         return this.username;
@@ -23,6 +45,26 @@ public class AccountCredentials {
         return this.role;
     }
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+    
     public void setUsername(String username) {
         this.username = username;
     }
@@ -34,4 +76,10 @@ public class AccountCredentials {
     public void setRole(String role) {
         this.role = role;
     }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
 }
