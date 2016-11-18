efdef06e36512c2560216c2ea1c59e8a8eeb8410
Louis Wilke <anmeldungs.mail@web.de>
2016-11-18 10:51:32 +0100

    removed old clock JavaScript

--- app/src/main/resources/static/resources/js/script.js
+++ app/src/main/resources/static/resources/js/script.js
@@ -1,17 +1,3 @@
-var timer = setInterval(clock,30000);
-
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
-
-function clock(){
-    now = new Date();
-    datetime = document.getElementById("clock").innerHTML;
-    document.getElementById("clock").innerHTML=(datetime.substr(0,datetime.indexOf(' '))
-    + " " + checkTime(now.getHours()+offsetHours) + ":" + checkTime(now.getMinutes()+offsetMinutes));
-}
-
 jQuery(document).ready(function(){
 
     // click sidebar toggle
