49b23ff861f4c2d721b0cd11f2ff2862ee6db005
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-11-29 12:02:11 +0100

    Add Tests for ProductCatalog

---
+++ app/src/test/java/winetavern/model/stock/ProductCatalogTests.java
@@ -0,0 +1,44 @@
+package winetavern.model.stock;
+
+import org.javamoney.moneta.Money;
+import org.junit.Before;
+import org.junit.Test;
+import org.salespointframework.catalog.Product;
+import org.salespointframework.quantity.Metric;
+import org.springframework.beans.factory.annotation.Autowired;
+import winetavern.AbstractIntegrationTests;
+
+import javax.validation.constraints.NotNull;
+
+import static org.salespointframework.core.Currencies.EURO;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by nwuensche on 29.11.16.
+ */
+public class ProductCatalogTests extends AbstractIntegrationTests {
+
+    @Autowired private ProductCatalog productCatalog;
+    private Product product;
+
+    @Before
+    public void before() {
+        product = new Product("Schnaps", Money.of(3, EURO), Metric.LITER);
+        product.addCategory(Category.LIQUOR.toString());
+        productCatalog.save(product);
+    }
+
+    @Test
+    public void setUpProductRight() {
+        assertThat(product.getCategories().stream().findFirst().get(), is(Category.LIQUOR.toString()));
+    }
+
+    @Test
+    public void findRightProduct() {
+        assertThat(productCatalog.findOne(product.getId().toString()), is(product));
+    }
+
+
+}
