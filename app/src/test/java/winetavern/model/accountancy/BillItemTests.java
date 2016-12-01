b89ee757a8ba06d9a2a63bfe71ad58b1e9bd790c
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-12-01 17:04:35 +0100

    Add Billitem tests

--- app/src/test/java/winetavern/model/accountancy/BillItemTests.java
+++ app/src/test/java/winetavern/model/accountancy/BillItemTests.java
@@ -1,19 +1,22 @@
 package winetavern.model.accountancy;
 
+import static org.salespointframework.core.Currencies.EURO;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+
 import org.javamoney.moneta.Money;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import winetavern.AbstractWebIntegrationTests;
 import winetavern.model.menu.DayMenuItem;
 
-import static org.salespointframework.core.Currencies.EURO;
 
 /**
  * @author Louis
  */
 
-public class BillItemTests extends AbstractWebIntegrationTests {
+public class BillItemTests {
+
     private DayMenuItem dayMenuItem = new DayMenuItem("Schinken", Money.of(3.56, EURO));
     private int quantity = 6;
     private BillItem billItem = new BillItem(dayMenuItem);
@@ -25,11 +28,17 @@ public class BillItemTests extends AbstractWebIntegrationTests {
 
     @Test
     public void getCorrectPrice() {
-        Assert.assertEquals(dayMenuItem.getPrice().multiply(quantity), billItem.getPrice());
+        assertEquals(dayMenuItem.getPrice().multiply(quantity), billItem.getPrice());
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void throwWhenNegativeQuantity() {
         billItem.changeQuantity(-1);
     }
+
+    @Test
+    public void testToString() {
+        assertThat(billItem.toString(), is("6 x Schinken"));
+    }
+
 }
