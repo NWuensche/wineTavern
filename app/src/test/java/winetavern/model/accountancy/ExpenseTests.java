8857d61ce1982ab46e336dde9ad35add13dd074d
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-12-02 11:24:10 +0100

    Add Expense tests. 100% CC accountancy

---
+++ app/src/test/java/winetavern/model/accountancy/ExpenseTests.java
@@ -0,0 +1,94 @@
+package winetavern.model.accountancy;
+
+import static org.salespointframework.core.Currencies.EURO;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+import static org.hamcrest.Matchers.lessThan;
+import static org.mockito.Mockito.*;
+
+import org.javamoney.moneta.Money;
+import org.junit.Before;
+import org.junit.Test;
+import org.salespointframework.useraccount.UserAccount;
+import winetavern.model.user.Employee;
+
+
+/**
+ * @author Niklas Wünsche
+ */
+
+public class ExpenseTests {
+
+    private Expense expense;
+    private Employee mockedEmployee;
+    private ExpenseGroup mockedExpenseGroup;
+    private String description;
+
+    @Before
+    public void before() {
+        mockedEmployee = mock(Employee.class);
+        mockedExpenseGroup = mock(ExpenseGroup.class);
+        description = "Text";
+
+        when(mockedExpenseGroup.getName()).thenReturn("Name");
+
+        expense = new Expense(Money.of(3, EURO), description, mockedEmployee, mockedExpenseGroup);
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throwIfEmployeeNull() {
+        expense = new Expense(Money.of(3, EURO), description, null, mockedExpenseGroup);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throwIfExpenseGroupNull() {
+        expense = new Expense(Money.of(3, EURO), description, mockedEmployee, null);
+    }
+
+    @Test
+    public void coverWhenAbrechnung() {
+        when(mockedExpenseGroup.getName()).thenReturn("Abrechnung");
+        expense = new Expense(Money.of(3, EURO), description, mockedEmployee, mockedExpenseGroup);
+
+        assertThat(expense.isCovered(), is(true));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void throwWhen2TimesCovered() {
+        expense.cover();
+        expense.cover();
+    }
+
+    @Test
+    public void isCoveredRight() {
+        assertThat(expense.isCovered(), is(false));
+
+        expense.cover();
+        assertThat(expense.isCovered(), is(true));
+    }
+
+    @Test
+    public void compareRight() {
+        Expense expense2 = setUpExpensesToCompare("Müller", "Schulz");
+
+        assertThat(expense.compareTo(expense2), lessThan(0));
+    }
+
+    private Expense setUpExpensesToCompare(String lastName1, String lastName2) {
+        UserAccount mockedUserAccount = mock(UserAccount.class);
+        UserAccount mockedUserAccount2 = mock(UserAccount.class);
+
+        when(mockedUserAccount.getLastname()).thenReturn(lastName1);
+        when(mockedUserAccount2.getLastname()).thenReturn(lastName2);
+
+        Employee mockedEmployee2 = mock(Employee.class);
+        Expense expense2 = new Expense(Money.of(3, EURO), "Abrechnung", mockedEmployee2, mockedExpenseGroup);
+
+        when(mockedEmployee.getUserAccount()).thenReturn(mockedUserAccount);
+        when(mockedEmployee2.getUserAccount()).thenReturn(mockedUserAccount2);
+
+        return expense2;
+    }
+
+}
