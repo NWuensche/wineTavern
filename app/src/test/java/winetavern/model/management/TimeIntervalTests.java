9528cf68f7144da7098565a0f046896bf55d32a9
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-12-02 13:49:01 +0100

    Update TimeIntervalTests

--- app/src/test/java/winetavern/model/management/TimeIntervalTests.java
+++ app/src/test/java/winetavern/model/management/TimeIntervalTests.java
@@ -1,23 +1,25 @@
 package winetavern.model.management;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.transaction.annotation.Transactional;
-import winetavern.AbstractWebIntegrationTests;
-
-import java.sql.Time;
 import java.time.LocalDateTime;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 /**
  * Test class for {@link TimeInterval}
  * @author Louis
  */
-@Transactional
-public class TimeIntervalTests extends AbstractWebIntegrationTests {
-    private LocalDateTime start = LocalDateTime.now();
-    private LocalDateTime end = start.plusHours(3);
+public class TimeIntervalTests {
+    private LocalDateTime start;
+    private LocalDateTime end;
+
+    @Before
+    public void before() {
+        start = LocalDateTime.now();
+        end = start.plusHours(3);
+    }
+
 
     @Test(expected = NullPointerException.class)
     public void throwWhenNullTime() {
@@ -35,9 +37,10 @@ public class TimeIntervalTests extends AbstractWebIntegrationTests {
         TimeInterval second = new TimeInterval(start.plusHours(1),start.plusHours(4));
         TimeInterval third = new TimeInterval(end.plusHours(2),end.plusHours(5));
 
-        assertTrue(first.intersects(second));
-        assertTrue(second.intersects(first));
-        assertFalse(first.intersects(third));
-        assertFalse(third.intersects(first));
+        assertThat(first.intersects(second), is(true));
+        assertThat(second.intersects(first), is(true));
+        assertThat(first.intersects(third), is(false));
+        assertThat(third.intersects(first), is(false));
     }
+
 }
