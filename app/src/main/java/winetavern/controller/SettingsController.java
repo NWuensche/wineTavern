33ff40a0f6748a6f69988f1d3d63b158267a81e3
SEV <severin.engert@mailbox.tu-dresden.de>
2016-12-02 11:22:13 +0100

    settings template + controller to change businessTime

--- app/src/main/java/winetavern/controller/SettingsController.java
+++ app/src/main/java/winetavern/controller/SettingsController.java
@@ -1,10 +1,17 @@
 package winetavern.controller;
 
+import lombok.NonNull;
+import org.salespointframework.time.BusinessTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author Louis
@@ -12,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SettingsController {
+    @NonNull @Autowired private BusinessTime businessTime;
+
     @RequestMapping("/admin/settings")
     public String showSettings(Model model) {
         return "settings";
@@ -19,7 +28,11 @@ public class SettingsController {
 
     @PostMapping("/admin/settings")
     public String setBusinessTime(@RequestParam String time) {
+        LocalDateTime newTime = LocalDateTime.parse(time, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+        LocalDateTime oldTime = businessTime.getTime();
 
-        return "redirect:";
+        Duration duration = Duration.between(oldTime,newTime);
+        businessTime.forward(duration);
+        return "redirect:/admin/settings";
     }
 }
