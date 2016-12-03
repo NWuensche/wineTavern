0e556504a0b8e22543d9bb4d4d7f817bf51f42c2
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-12-03 19:34:50 +0100

    Replace DateParameter Getter/Setter with Lombok

--- app/src/main/java/winetavern/model/DateParameter.java
+++ app/src/main/java/winetavern/model/DateParameter.java
@@ -1,43 +1,24 @@
 package winetavern.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Calendar;
-import java.util.Date;
 
 /**
- * Created by Michel on 11/5/2016.
+ * @author Michel
  */
+
+@Getter
+@Setter
 public class DateParameter {
+
     private int day;
     private int month;
     private int year;
 
-    public int getDay() {
-        return day;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setDay(int day) {
-        this.day = day;
-    }
-
-    public void setMonth(int month) {
-        this.month = month;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
     public LocalDate getDate() {
         return LocalDate.of(year, month, day);
     }
+
 }
