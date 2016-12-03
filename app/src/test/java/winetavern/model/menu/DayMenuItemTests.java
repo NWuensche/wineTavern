abf3e8e9e08ba3a6520f7224676e8bb898b8b110
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-12-03 20:27:34 +0100

    Add test for add and remove dayMenu

--- app/src/test/java/winetavern/model/menu/DayMenuItemTests.java
+++ app/src/test/java/winetavern/model/menu/DayMenuItemTests.java
@@ -1,6 +1,11 @@
 package winetavern.model.menu;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+import static org.mockito.Mockito.*;
+
 import org.javamoney.moneta.Money;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.salespointframework.core.Currencies.EURO;
@@ -11,9 +16,36 @@ import static org.salespointframework.core.Currencies.EURO;
 
 public class DayMenuItemTests {
 
+    private DayMenuItem dayMenuItem;
+
+    @Before
+    public void before() {
+        dayMenuItem = new DayMenuItem("Name", "Description", Money.of(3, EURO), 6.0);
+    }
+
     @Test
-    public void createItemWorks() {
-        new DayMenuItem("Name", "Description", Money.of(3, EURO), 6.0);
+    public void addRight() {
+        addDayMenus();
+        assertThat(dayMenuItem.getDayMenus().size(), is(2));
+    }
+
+    private DayMenu addDayMenus() {
+        DayMenu mockedDayMenu = mock(DayMenu.class);
+        DayMenu mockedDayMenu2 = mock(DayMenu.class);
+        dayMenuItem.addDayMenu(mockedDayMenu);
+        dayMenuItem.addDayMenu(mockedDayMenu2);
+
+        return mockedDayMenu;
+    }
+
+    @Test
+    public void removeRight() {
+        DayMenu mockedDayMenu = addDayMenus();
+
+        dayMenuItem.removeDayMenu(mockedDayMenu);
+
+        assertThat(dayMenuItem.getDayMenus().size(), is(1));
+        assertThat(dayMenuItem.getDayMenus().contains(mockedDayMenu), is(false));
     }
 
 }
