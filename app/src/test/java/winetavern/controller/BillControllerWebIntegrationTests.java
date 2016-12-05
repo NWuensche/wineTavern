04263ed2215967bd4ccc5afa1b83d49ec84df256
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-12-05 12:05:49 +0100

    Add BillController tests

--- app/src/test/java/winetavern/controller/BillControllerWebIntegrationTests.java
+++ app/src/test/java/winetavern/controller/BillControllerWebIntegrationTests.java
@@ -1,8 +1,51 @@
 package winetavern.controller;
 
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.RequestBuilder;
+import winetavern.AbstractWebIntegrationTests;
+import winetavern.model.user.Roles;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 /**
  * @author Niklas Wünsche
  */
 
-public class BillControllerWebIntegrationTests {
+public class BillControllerWebIntegrationTests extends AbstractWebIntegrationTests {
+
+    @Autowired private BillController billController;
+
+    @Test
+    public void serviceAndAdminAuthorized() throws Exception {
+        RequestBuilder serviceRequest = post("/service/bills")
+                .with(user("service").roles(Roles.SERVICE.getRealNameOfRole()));
+
+        mvc.perform(serviceRequest)
+                .andExpect(authenticated());
+
+        RequestBuilder adminRequest = post("/service/bills")
+                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()));
+
+        mvc.perform(adminRequest)
+                .andExpect(authenticated());
+    }
+
+    @Test
+    public void showBillsModelAttributesRight() throws Exception{
+        RequestBuilder request = post("/service/bills")
+                .with(user("service").roles(Roles.SERVICE.getRealNameOfRole()));
+
+        mvc.perform(request)
+                .andExpect(model().attributeExists("active"))
+                .andExpect(view().name("bills"));
+    }
+
+
+
 }
