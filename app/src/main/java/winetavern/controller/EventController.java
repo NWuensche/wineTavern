89855ca01ee52453e7a3b74798b907b998f89fef
SEV <severin.engert@mailbox.tu-dresden.de>
2016-12-07 20:33:32 +0100

    now you can add a external person for an event

--- app/src/main/java/winetavern/controller/EventController.java
+++ app/src/main/java/winetavern/controller/EventController.java
@@ -10,6 +10,7 @@ import winetavern.model.management.Event;
 import winetavern.model.management.EventCatalog;
 import winetavern.model.management.TimeInterval;
 import winetavern.model.user.External;
+import winetavern.model.user.ExternalManager;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -25,11 +26,13 @@ import static org.salespointframework.core.Currencies.EURO;
 @Controller
 public class EventController {
     private final EventCatalog eventCatalog;
+    private final ExternalManager externals;
     private final BusinessTime time;
 
     @Autowired
-    public EventController(EventCatalog eventCatalog, BusinessTime time) {
+    public EventController(EventCatalog eventCatalog, ExternalManager externals, BusinessTime time) {
         this.eventCatalog = eventCatalog;
+        this.externals = externals;
         this.time = time;
     }
 
@@ -41,14 +44,17 @@ public class EventController {
     }
 
     @RequestMapping("/admin/events/add")
-    public String showAddEventTemplate(){
+    public String showAddEventTemplate(Model model){
+        model.addAttribute("externals",externals.findAll());
         return "addevent";
     }
 
     @PostMapping("/admin/events/add")
     public String addEvent(@RequestParam String name, @RequestParam String desc, @RequestParam String date,
-                           @RequestParam String price) { //TODO @RequestParam String externalId (== 0 if new External)
-                                                         //TODO @RequestParam String name, wage
+                           @RequestParam String price, @RequestParam String person, @RequestParam String personname,
+                           @RequestParam String
+                                       personwage) {
+                                                         //TODO stimmt das alles so?
         if (!date.isEmpty()) {
             DateTimeFormatter parser = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
             String[] splittedDate = date.split("\\s-\\s");
@@ -56,8 +62,18 @@ public class EventController {
                 LocalDateTime start = LocalDateTime.parse(splittedDate[0], parser);
                 LocalDateTime end = LocalDateTime.parse(splittedDate[1], parser);
 
+                External externalPerson;
+                if(person.equals("0")){
+                    externalPerson = new External(personname,Money.of(Float.valueOf(personwage),EURO)); // das mit
+                    // dem float geht nicht wieso auch immer?!
+
+                } else {
+                    externalPerson = externals.findByName(person).get();
+                }
+
+
                 eventCatalog.save(new Event(name, Money.of((Float.valueOf(price)), EURO),
-                                  new TimeInterval(start, end), desc, new External("der Neue", Money.of(180, EURO))));
+                                  new TimeInterval(start, end), desc, externalPerson));
             }
         }
         return "redirect:/admin/events";
