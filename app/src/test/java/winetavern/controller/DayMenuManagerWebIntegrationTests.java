c688599d2d9a2b1bb6a219c54eba276045c50805
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-12-08 09:07:29 +0100

    Add DayMenuManager Tests.

--- app/src/test/java/winetavern/controller/DayMenuManagerWebIntegrationTests.java
+++ app/src/test/java/winetavern/controller/DayMenuManagerWebIntegrationTests.java
@@ -5,9 +5,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import org.junit.Before;
 import org.springframework.validation.BindException;
 import winetavern.AbstractWebIntegrationTests;
 import org.junit.Test;
@@ -23,14 +25,43 @@ import winetavern.model.user.Roles;
 import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Optional;
 
 /**
  * Created by michel on 13.11.16.
  */
 @Transactional
 public class DayMenuManagerWebIntegrationTests extends AbstractWebIntegrationTests {
-    @Autowired DayMenuManager dayMenuManager;
-    @Autowired DayMenuRepository dayMenuRepository;
+    @Autowired private DayMenuManager dayMenuManager;
+    @Autowired private DayMenuRepository dayMenuRepository;
+
+    private DayMenu dayMenu;
+
+    @Before
+    public void before() {
+        dayMenu = new DayMenu(LocalDate.of(2016,11,11));
+        dayMenuRepository.save(dayMenu);
+    }
+
+    @Test
+    public void showMenusRight() throws Exception {
+        RequestBuilder request = post("/admin/menu/show")
+                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()));
+
+        mvc.perform(request)
+                .andExpect(model().attributeExists("menus"))
+                .andExpect(view().name("daymenulist"));
+    }
+
+    @Test
+    public void addMenuGetRight() throws Exception {
+        RequestBuilder request = get("/admin/menu/add")
+                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()));
+
+        mvc.perform(request)
+                .andExpect(model().attributeExists("date"))
+                .andExpect(view().name("addmenu"));
+    }
 
     @Test
     public void createDayMenuWithWrongYear() throws Exception {
@@ -61,6 +92,41 @@ public class DayMenuManagerWebIntegrationTests extends AbstractWebIntegrationTes
             }
         });
         assertThat(isDateInRepo[0], is(true));
+    }
+
+    @Test
+    public void removeDayMenuRight() throws Exception {
+        DayMenu willBeDeleted = new DayMenu(LocalDate.now());
+        dayMenuRepository.save(willBeDeleted);
+
+        RequestBuilder request = post("/admin/menu/remove")
+                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()))
+                .param("daymenuid", willBeDeleted.getId().toString());
+
+        mvc.perform(request)
+                .andExpect(view().name("daymenulist"));
+
+        assertThat(dayMenuRepository.findOne(willBeDeleted.getId()), is(Optional.empty()));
+    }
+
+    @Test
+    public void editRight() throws Exception {
+        RequestBuilder request = post("/admin/menu/edit/" + dayMenu.getId())
+                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()));
+
+        mvc.perform(request)
+                .andExpect(view().name("editdaymenu"))
+                .andExpect(model().attribute("daymenu", dayMenu))
+                .andExpect(model().attributeExists("stock"));
+    }
+
+    @Test
+    public void getDayMenuByDayRight() throws Exception {
+        RequestBuilder request = post("/admin/menu/getDayMenuByDay")
+                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()));
+
+        mvc.perform(request)
+                .andExpect(view().name("daymenu"));
     }
 
 }
