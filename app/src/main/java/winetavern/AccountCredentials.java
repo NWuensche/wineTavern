a3fed42c9fe0b6d4e06e41fdc0894e441b340af1
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-12-10 16:05:31 +0100

    Add Lombok to AccountCredentials

--- app/src/main/java/winetavern/AccountCredentials.java
+++ app/src/main/java/winetavern/AccountCredentials.java
@@ -1,11 +1,15 @@
 package winetavern;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Class to store credentials from the login page.
  * @author michel
  */
-
 // TODO Make this an inner class of EmployeeManagerController
+@Getter
+@Setter
 public class AccountCredentials {
 
     private String personTitle;
@@ -16,70 +20,5 @@ public class AccountCredentials {
     private String password;
     private String role;
     private String address;
-
-    public String getPersonTitle() {
-        return personTitle;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getBirthday() {
-        return birthday;
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public String getRole() {
-        return this.role;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setPersonTitle(String per) {
-        this.personTitle = per;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setBirthday(String birthday) {
-        this.birthday = birthday;
-    }
-    
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
 
 }
