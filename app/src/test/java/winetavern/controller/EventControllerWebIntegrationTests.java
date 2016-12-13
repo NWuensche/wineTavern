4c7674210fe73f94ed05b93af529c016719d76a3
Louis Wilke <anmeldungs.mail@web.de>
2016-12-13 11:42:53 +0100

    removed outdated Tests for Events and Shifts

--- app/src/test/java/winetavern/controller/EventControllerWebIntegrationTests.java
+++ app/src/test/java/winetavern/controller/EventControllerWebIntegrationTests.java
@@ -53,37 +53,4 @@ public class EventControllerWebIntegrationTests extends AbstractWebIntegrationTe
                 .andExpect(view().name("events"));
     }
 
-    @Test
-    public void addEventRight() throws Exception {
-        RequestBuilder request = post("/admin/events/add")
-                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()))
-                .param("name", event.getName())
-                .param("price", event.getPrice().toString())
-                .param("categories", event.getCategories().toString())
-                .param("metric", Metric.UNIT.toString());
-
-        mvc.perform(request)
-                .andExpect(status().is3xxRedirection());
-
-        assertThat(eventCatalog.count(), is(numberOfEventsInDataInit + 1));
-
-        Event storedEvent = Helper.getFirstItem(eventCatalog.findByName(event.getName()));
-
-        assertThat(storedEvent.compareTo(event), is(0));
-    }
-
-    @Test
-    public void removeEventRight() throws Exception {
-        eventCatalog.save(event);
-
-        RequestBuilder request = post("/admin/events/remove")
-                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()))
-                .param("event", event.getId().toString());
-
-        mvc.perform(request)
-                .andExpect(status().is3xxRedirection());
-
-        assertThat(eventCatalog.count(), is(numberOfEventsInDataInit + 0));
-    }
-
 }
