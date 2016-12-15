2a065138b2f873233c58781a76224c57c75b1172
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2016-12-15 09:39:15 +0100

    Add Vintner Tests

---
+++ app/src/test/java/winetavern/model/user/VintnerTests.java
@@ -0,0 +1,40 @@
+package winetavern.model.user;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author Niklas Wünsche
+ */
+
+public class VintnerTests {
+
+    @Test(expected = NullPointerException.class)
+    public void throwWhenVintnerWithNoName() {
+        new Vintner(null, 1);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throwWhenVintnerWithEmptyName() {
+        new Vintner("", 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwWhenVintnerWithNegativePosition() {
+        new Vintner("Vintner", -1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwWhenVintnerSetNegativePosition() {
+        new Vintner("Vintner", 1).setPosition(-1);
+    }
+
+    @Test
+    public void toStringRight() {
+        Vintner vintner = new Vintner("Vintner", 1);
+        assertThat(vintner.toString(), is("Vintner"));
+    }
+
+}
