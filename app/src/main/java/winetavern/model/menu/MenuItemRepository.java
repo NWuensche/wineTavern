fae8042b1952c96195bfd65a8b0bb598c4bf6993
Michel Kunkler <michel.kunkler@gmail.com>
2016-12-19 11:49:12 +0100

    delete unused model interface

--- app/src/main/java/winetavern/model/menu/MenuItemRepository.java
+++
@@ -1,9 +0,0 @@
-package winetavern.model.menu;
-
-import org.salespointframework.core.SalespointRepository;
-
-/**
- * @author Michel
- */
-public interface MenuItemRepository extends SalespointRepository<DayMenuItem, Long> {
-}
