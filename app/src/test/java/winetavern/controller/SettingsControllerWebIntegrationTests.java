e5d98b14ac6feb040c52216f25c0ba3e665c7b2a
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2016-12-30 16:20:10 +0100

    Refactor SettingsControllerTests

--- app/src/test/java/winetavern/controller/SettingsControllerWebIntegrationTests.java
+++ app/src/test/java/winetavern/controller/SettingsControllerWebIntegrationTests.java
@@ -9,6 +9,7 @@ import winetavern.model.user.Roles;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.AnyOf.*;
@@ -17,6 +18,8 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static winetavern.controller.RequestHelper.buildGetAdminRequest;
+import static winetavern.controller.RequestHelper.buildPostAdminRequest;
 
 /**
  * @author Niklas Wünsche
@@ -26,25 +29,21 @@ public class SettingsControllerWebIntegrationTests extends AbstractWebIntegratio
     @Autowired BusinessTime businessTime;
 
     @Test
-    public void showSettingsRight() throws Exception{
-        RequestBuilder request = get("/admin/settings")
-                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()))
-                .param("reservationtime", "2016/11/11 11:11");
-
-        mvc.perform(request)
+    public void showSettingsRight() throws Exception {
+        mvc.perform(buildGetAdminRequest("/admin/settings"))
                 .andExpect(view().name("settings"));
     }
 
     @Test
-    public void setBusinessTimeCorrect() throws Exception{
-        RequestBuilder request = post("/admin/settings")
-                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()))
-                .param("time", "11.11.2016 11:11");
+    public void setBusinessTimeCorrect() throws Exception {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("time", "11.11.2016 11:11");
 
-        mvc.perform(request)
+        mvc.perform(buildPostAdminRequest("/admin/settings", params))
                 .andExpect(status().is3xxRedirection());
 
-        assertThat(Duration.between(businessTime.getTime(), LocalDateTime.of(2016, 11, 11, 11, 11)).getSeconds(), (anyOf(is(-1l), is(0l), is(1l))));
+        assertThat(Duration.between(businessTime.getTime(), LocalDateTime.of(2016, 11, 11, 11, 11)).getSeconds(),
+                (anyOf(is(-1l), is(0l), is(1l))));
     }
 
 }
