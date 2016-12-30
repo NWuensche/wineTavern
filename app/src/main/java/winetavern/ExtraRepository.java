1ced7031f3bc6ad79a4502ccce72841c1626ae39
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2016-12-30 11:21:53 +0100

    Add new Repository

--- app/src/main/java/winetavern/ExtraRepository.java
+++ app/src/main/java/winetavern/ExtraRepository.java
@@ -1,7 +1,27 @@
 package winetavern;
 
+import org.salespointframework.core.SalespointRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 /**
  * @author Niklas Wünsche
  */
-public interface ExtraRepository {
+@NoRepositoryBean
+public interface ExtraRepository<T, I extends Serializable> extends SalespointRepository<T, I> {
+
+    default List<T> convertToList() {
+        return StreamSupport.stream(findAll().spliterator(), false)
+                .collect(Collectors.toList());
+    }
+
+    default T getFirstItem() {
+        return convertToList().get(0);
+    }
+
 }
