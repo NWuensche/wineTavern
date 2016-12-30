df8cdae9e8c4525403d1606a466a2303449274ff
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2016-12-30 15:15:45 +0100

    Make MVC in DayMenuManager nicer

--- app/src/main/java/winetavern/model/menu/DayMenuRepository.java
+++ app/src/main/java/winetavern/model/menu/DayMenuRepository.java
@@ -1,6 +1,8 @@
 package winetavern.model.menu;
 
 import org.salespointframework.core.SalespointRepository;
+import winetavern.ExtraRepository;
+
 import java.time.LocalDate;
 import java.util.Optional;
 
@@ -8,6 +10,6 @@ import java.util.Optional;
 /**
  * @author Michel
  */
-public interface DayMenuRepository extends SalespointRepository<DayMenu, Long> {
+public interface DayMenuRepository extends ExtraRepository<DayMenu, Long> {
     Optional<DayMenu> findByDay(LocalDate day);
 }
--- app/src/test/java/winetavern/controller/DayMenuManagerWebIntegrationTests.java
+++ app/src/test/java/winetavern/controller/DayMenuManagerWebIntegrationTests.java
@@ -8,6 +8,8 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static winetavern.controller.RequestHelper.buildGetAdminRequest;
+import static winetavern.controller.RequestHelper.buildPostAdminRequest;
 
 import org.javamoney.moneta.Money;
 import org.junit.Before;
@@ -36,6 +38,7 @@ import winetavern.model.user.VintnerTests;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -79,11 +82,8 @@ public class DayMenuManagerWebIntegrationTests extends AbstractWebIntegrationTes
     }
 
     private void addVintnerDays() {
-        RequestBuilder request = post("/admin/events")
-                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()));
-
         try {
-            mvc.perform(request);
+            mvc.perform(buildPostAdminRequest("/admin/events"));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -91,30 +91,24 @@ public class DayMenuManagerWebIntegrationTests extends AbstractWebIntegrationTes
 
     @Test
     public void showMenusRight() throws Exception {
-        RequestBuilder request = post("/admin/menu/show")
-                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()));
-
-        mvc.perform(request)
+        mvc.perform(buildPostAdminRequest("/admin/menu/show"))
                 .andExpect(model().attributeExists("menus"))
                 .andExpect(view().name("daymenulist"));
     }
 
     @Test
     public void addMenuGetRight() throws Exception {
-        RequestBuilder request = get("/admin/menu/add")
-                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()));
-
-        mvc.perform(request)
+        mvc.perform(buildGetAdminRequest("/admin/menu/add"))
                 .andExpect(view().name("addmenu"));
     }
 
     @Test
     public void createDayMenuWithWrongYear() throws Exception {
-        RequestBuilder request = post("/admin/menu/add").with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()))
-                .param("date", "30.11.wrong");
+        HashMap<String, String> params = new HashMap<>();
+        params.put("date", "30.11.wrong");
 
         try {
-            mvc.perform(request);
+            mvc.perform(buildPostAdminRequest("/admin/menu/add", params));
             fail();
         }
         catch (NestedServletException e) {
@@ -125,22 +119,16 @@ public class DayMenuManagerWebIntegrationTests extends AbstractWebIntegrationTes
 
     @Test
     public void createDayMenuWithRealDate() throws Exception {
-        RequestBuilder request = post("/admin/menu/add").with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()))
-                .param("date", "04.09.2015");
-        mvc.perform(request);
+        HashMap<String, String> params = new HashMap<>();
+        params.put("date", "04.09.2015");
+
+        mvc.perform(buildPostAdminRequest("/admin/menu/add", params));
 
         LocalDate givenDate = LocalDate.of(2015, 9, 4);
 
-        boolean[] isDateInRepo = {false};
-        Iterable<DayMenu> allDayMenus = dayMenuRepository.findAll();
-
-        allDayMenus.forEach(dayMenu -> {
-            LocalDate date = dayMenu.getDay();
-            if(date.equals(givenDate)) {
-                isDateInRepo[0] = true;
-            }
-        });
-        assertThat(isDateInRepo[0], is(true));
+        assertTrue(dayMenuRepository
+                .stream()
+                .anyMatch(date -> date.getDay().equals(givenDate)));
     }
 
     @Test
@@ -148,11 +136,10 @@ public class DayMenuManagerWebIntegrationTests extends AbstractWebIntegrationTes
         DayMenu willBeDeleted = new DayMenu(LocalDate.now());
         dayMenuRepository.save(willBeDeleted);
 
-        RequestBuilder request = post("/admin/menu/remove")
-                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()))
-                .param("daymenuid", willBeDeleted.getId().toString());
+        HashMap<String, String> params = new HashMap<>();
+        params.put("daymenuid", willBeDeleted.getId().toString());
 
-        mvc.perform(request)
+        mvc.perform(buildPostAdminRequest("/admin/menu/remove", params))
                 .andExpect(status().is3xxRedirection());
 
         assertThat(dayMenuRepository.findOne(willBeDeleted.getId()), is(Optional.empty()));
@@ -160,10 +147,7 @@ public class DayMenuManagerWebIntegrationTests extends AbstractWebIntegrationTes
 
     @Test
     public void editRight() throws Exception {
-        RequestBuilder request = post("/admin/menu/edit/" + dayMenu.getId())
-                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()));
-
-        mvc.perform(request)
+        mvc.perform(buildPostAdminRequest("/admin/menu/edit/" + dayMenu.getId()))
                 .andExpect(view().name("editdaymenu"))
                 .andExpect(model().attribute("daymenu", dayMenu))
                 .andExpect(model().attributeExists("stock"));
@@ -171,10 +155,7 @@ public class DayMenuManagerWebIntegrationTests extends AbstractWebIntegrationTes
 
     @Test
     public void pdfRight() throws Exception {
-        RequestBuilder request = post("/admin/menu/print/" + dayMenu.getId())
-                .with(user("admin").roles(Roles.ADMIN.getRealNameOfRole()));
-
-        mvc.perform(request)
+        mvc.perform(buildPostAdminRequest("/admin/menu/print/" + dayMenu.getId()))
                 .andExpect(view().name("daymenupdf"));
     }
 
